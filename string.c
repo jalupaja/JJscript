550a86d88eaa9efac19ec52f82e3f379453da7df
Jakob Pistohl <gh@jp-bits.de>
2025-01-02 00:08:55 +0100

    Change string.h to use pointers; Add more error handling to string_create

--- string.c
+++ string.c
@@ -4,96 +4,114 @@
 #include <stdlib.h>
 #include <string.h>
 
+// TODO need to free on realloc?
 #define INITIAL_CAPACITY 16
 
-string string_create(const char *init) {
-  string str;
-  str.length = init ? strlen(init) : 0;
-  str.capacity =
-      (str.length + 1 > INITIAL_CAPACITY) ? str.length + 1 : INITIAL_CAPACITY;
-  str.data = (char *)malloc(str.capacity);
-  if (init) {
-    strcpy(str.data, init);
-  } else {
-    str.data[0] = '\0';
+#define DEBUG 0
+
+string *string_create(const char *init) {
+  string *str = (string *)malloc(sizeof(string));
+  if (!str) {
+    fprintf(stderr, "Memory allocation failed for string struct\n");
+    exit(EXIT_FAILURE);
   }
+  str->length = init ? strlen(init) : 0;
+  str->capacity =
+      (str->length + 1 > INITIAL_CAPACITY) ? str->length + 1 : INITIAL_CAPACITY;
+
+  str->data = (char *)malloc(str->capacity);
+  if (!str->data) {
+    fprintf(stderr, "Memory allocation failed for string data\n");
+    free(str);
+    exit(EXIT_FAILURE);
+  }
+  if (DEBUG)
+    printf("Allocating string data at %p with capacity %zu - '%s'\n",
+           (void *)str->data, str->capacity, init);
+
+  if (init) {
+    strcpy(str->data, init);
+  } else {
+    str->data[0] = '\0';
+  }
+
   return str;
 }
 
-ssize_t string_len(string str) { return str.length; }
+ssize_t string_len(string *str) { return str->length; }
 
-void string_append_char(string str, const char suffix) {
-  if (str.length + 1 >= str.capacity) {
+void string_append_char(string *str, const char suffix) {
+  if (str->length + 1 >= str->capacity) {
 
-    str.capacity *= 2;
-    str.data = (char *)realloc(str.data, str.capacity);
+    str->capacity *= 2;
+    str->data = (char *)realloc(str->data, str->capacity);
   }
 
-  str.data[str.length] = suffix;
-  str.length++;
-  str.data[str.length] = '\0';
+  str->data[str->length] = suffix;
+  str->length++;
+  str->data[str->length] = '\0';
 }
 
-void string_append_chars(string str, const char *suffix) {
+void string_append_chars(string *str, const char *suffix) {
   size_t suffix_len = strlen(suffix);
 
-  if (str.length + suffix_len >= str.capacity) {
-    while (str.length + suffix_len >= str.capacity) {
-      str.capacity *= 2;
+  if (str->length + suffix_len >= str->capacity) {
+    while (str->length + suffix_len >= str->capacity) {
+      str->capacity *= 2;
     }
-    str.data = (char *)realloc(str.data, str.capacity);
+    str->data = (char *)realloc(str->data, str->capacity);
   }
 
-  strcat(str.data, suffix);
-  str.length += suffix_len;
+  strcat(str->data, suffix);
+  str->length += suffix_len;
 }
 
-void string_remove_chars_from_end(string str, int amount) {
-  ssize_t new_len = str.length - amount;
+void string_remove_chars_from_end(string *str, int amount) {
+  ssize_t new_len = str->length - amount;
   if (new_len < 0)
     new_len = 0;
-  str.data[new_len] = '\0';
-  str.length = new_len;
+  str->data[new_len] = '\0';
+  str->length = new_len;
 }
 
-void string_append_string(string str1, string str2) {
-  if (str1.length + str2.length >= str1.capacity) {
-    while (str1.length + str2.length >= str1.capacity) {
-      str1.capacity *= 2;
+void string_append_string(string *str1, string *str2) {
+  if (str1->length + str2->length >= str1->capacity) {
+    while (str1->length + str2->length >= str1->capacity) {
+      str1->capacity *= 2;
     }
-    str1.data = (char *)realloc(str1.data, str1.capacity);
+    str1->data = (char *)realloc(str1->data, str1->capacity);
   }
 
-  strcat(str1.data, str2.data);
-  str1.length += str2.length;
+  strcat(str1->data, str2->data);
+  str1->length += str2->length;
 }
 
-string string_substring(string str, size_t start, size_t end) {
-  if (start > end || end > str.length) {
+string *string_substring(string *str, size_t start, size_t end) {
+  if (start > end || end > str->length) {
     fprintf(stderr, "Invalid substring range.\n");
     exit(EXIT_FAILURE);
   }
-  string sub;
-  sub.length = end - start;
-  sub.capacity = sub.length + 1;
-  sub.data = (char *)malloc(sub.capacity);
-  strncpy(sub.data, str.data + start, sub.length);
-  sub.data[sub.length] = '\0';
+  string *sub = (string *)malloc(sizeof(string));
+  sub->length = end - start;
+  sub->capacity = sub->length + 1;
+  sub->data = (char *)malloc(sub->capacity);
+  strncpy(sub->data, str->data + start, sub->length);
+  sub->data[sub->length] = '\0';
   return sub;
 }
 
-char string_char_at(string str, size_t index) {
-  if (index >= str.length) {
+char string_char_at(string *str, size_t index) {
+  if (index >= str->length) {
     fprintf(stderr, "Index out of bounds.\n");
     exit(EXIT_FAILURE);
   }
-  return str.data[index];
+  return str->data[index];
 }
 
-char *string_get_chars(string str) { return str.data; }
+char *string_get_chars(string *str) { return str->data; }
 
-int string_cmp(string str1, string str2) {
-  return strcmp(str1.data, str2.data);
+int string_cmp(string *str1, string *str2) {
+  return strcmp(str1->data, str2->data);
 }
 
 void string_free(string *str) {
--- string.h
+++ string.h
@@ -9,15 +9,15 @@ typedef struct {
   size_t capacity;
 } string;
 
-string string_create(const char *init);
-void string_append_char(string str, const char suffix);
-void string_append_chars(string str, const char *suffix);
-void string_remove_chars_from_end(string str, int amount);
-void string_append_string(string str1, string str2);
-string string_substring(string str, size_t start, size_t end);
-char string_char_at(string str, size_t index);
-char *string_get_chars(string str);
-int string_cmp(string str1, string str2);
+string *string_create(const char *init);
+void string_append_char(string *str, const char suffix);
+void string_append_chars(string *str, const char *suffix);
+void string_remove_chars_from_end(string *str, int amount);
+void string_append_string(string *str1, string *str2);
+string *string_substring(string *str, size_t start, size_t end);
+char string_char_at(string *str, size_t index);
+char *string_get_chars(string *str);
+int string_cmp(string *str1, string *str2);
 void string_free(string *str);
 
 #endif // STRING_H
